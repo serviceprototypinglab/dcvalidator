4ed740710df9ccc24b20872ea6f8d6698989c51f
dagh <dagh@zhaw.ch>
2019-08-22 13:37:23 +0200

    I really should've committed this when I finished it...

--- web-app/server/server.py
+++ web-app/server/server.py
@@ -4,37 +4,18 @@ from flask import Flask, flash, request, redirect, url_for, session
 from flask_restful import Resource, Api
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
-import logging
 
-logging.basicConfig(level=logging.INFO)
-
-# logger = logging.getLogger('HELLO WORLD')
 
 
 
 UPLOAD_FOLDER = './'
-# ALLOWED_EXTENSIONS = set(['yml', 'yaml'])
+ALLOWED_EXTENSIONS = set(['yml', 'yaml'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 api = Api(app)
 CORS(app)
-# @app.route('/upload', methods=['POST'])
-# def fileUpload():
-#     target=os.path.join(UPLOAD_FOLDER,'docker-compose-file')
-#     if not os.path.isdir(target):
-#         os.mkdir(target)
-#     logger.info("welcome to upload`")
-#     file = request.files['file'] 
-#     filename = secure_filename(file.filename)
-#     destination="/".join([target, filename])
-#     file.save(destination)
-#     session['uploadFilePath']=destination
-#     response="Whatever you wish too return"
-#     return response
 class fileUpload(Resource):
-    def get(self):
-        return {'hello': 'world'}
     
     def post(self):
         target=os.path.join(UPLOAD_FOLDER,'docker-compose-file')
@@ -42,14 +23,9 @@ class fileUpload(Resource):
         if not os.path.isdir(target):
             os.mkdir(target)
         print("dir ", str(target), " created")
-        # logger.info("welcome to upload`")
-        # ploaded_files = flask.request.files.getlist("file[]")
         Dfiles = request.files.getlist('file[]')
         print("files: ", Dfiles)
-        # logger.info(str(type(files)))
         for f in Dfiles:
-            # print(f)
-            # # logger.info(str(f))
             filename = secure_filename(f.filename)
             destination="/".join([target, filename])
             print("for: ", f, "\n", "name: ", filename, "\n", "destination: ", destination)
@@ -60,11 +36,6 @@ class fileUpload(Resource):
         return response
 
 api.add_resource(fileUpload, '/upload')
-
-# @app.route('/test', methods=['POST'])
-# def test():
-#     response="Whatever you wish too return"
-#     return response
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
